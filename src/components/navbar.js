ac07727d178b4e43c6a5261e014a6c28db7a2737
Arya Krisna Putra <aryakrisna07@gmail.com>
2026-06-13 21:12:46 +0700

    feat: add blog detail

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ const NAV_LINKS = [
   {label: 'Home', href: '/#home'},
   {label: 'About', href: '/#about'},
   {label: 'Projects', href: '/projects'},
-  {label: 'Blog', href: '/blogs'},
+  {label: 'Blogs', href: '/blogs'},
   {label: 'Contact', href: '/#contact'},
 ];
 
